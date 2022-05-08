cdfebe06e089dd006fc1ee477b8897d5d01ba5d7
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2022-05-08 11:31:11 +0900

    스프링 시큐리티 추가

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+/**
+ * 스프링 시큐리티 설정
+ */
+@Configuration      // 스프링 환경설정 애노테이션
+@EnableWebSecurity  // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애노테이션
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 모든 인증되지 않은 요청을 허락한다는 의미
+        http.authorizeRequests().antMatchers("/**").permitAll()
+                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+    }
+}
