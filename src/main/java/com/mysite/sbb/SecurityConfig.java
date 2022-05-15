094a4e2571a35f49805c4960020411d6931c6fe8
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2022-05-15 16:44:37 +0900

    로그인과 로그아웃 1. 스프링 시큐리티를 이용하여 데이터베이스에 존재하지 않는 회원은 로그인 불가(회원가입 후 이용가능) 2. 로그인 시 로그아웃버튼 네비게이션바에 생성 클릭시 로그아웃

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -1,30 +1,45 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.user.UserSecutiryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * 스프링 시큐리티 설정
  */
+@RequiredArgsConstructor
 @Configuration      // 스프링 환경설정 애노테이션
 @EnableWebSecurity  // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애노테이션
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserSecutiryService userSecutiryService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 모든 인증되지 않은 요청을 허락한다는 의미
         http.authorizeRequests().antMatchers("/**").permitAll()
                 .and().csrf().ignoringAntMatchers("/h2-console/**")
-                .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+                .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and().formLogin().loginPage("/user/login").defaultSuccessUrl("/")
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/user/logout")).logoutSuccessUrl("/").invalidateHttpSession(true);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userSecutiryService).passwordEncoder(passwordEncoder());
     }
 }
