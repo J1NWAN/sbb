bb48386dd198f9a8494dbfd102a0506c7163ec07
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2022-05-22 10:51:44 +0900

    Specification을 사용한 검색기능 구현

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -19,4 +20,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     // 모든 질문을 조회
     Page<Question> findAll(Pageable pageable);
+
+    // 모든 질문 검색 기능
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
