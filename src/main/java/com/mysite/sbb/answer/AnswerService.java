321a7ac79a9078357f7bf2448b05556ed677fe47
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2022-05-22 10:40:57 +0900

    질문과 답변을 마크다운 문법으로 작성가능하도록 구현

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -16,7 +16,7 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
@@ -24,6 +24,7 @@ public class AnswerService {
         answer.setAuthor(author);
 
         this.answerRepository.save(answer);
+        return answer;
     }
 
     public Answer getAnswer(Integer id) {
